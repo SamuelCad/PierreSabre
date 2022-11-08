56ddb4d14a3d4447e0428c0ba1683ae5b1bc5456
SamuelCad <samuel.cadasse@gmail.com>
2022-11-08 23:58:43 +0100

    finTP4

--- src/histoire/HistoireTP4.java
+++ src/histoire/HistoireTP4.java
@@ -7,20 +7,26 @@ import personnages.Ronin;
 public class HistoireTP4 {
 
 	public static void main(String[] args) {
-//		Humain prof= new Humain("Prof", "kombucha", 24);
-//		prof.direBonjour();
-//		prof.acheter( "kombucha", 12);
-//		prof.acheter( "kombucha", 11);
+		Humain prof= new Humain("Prof", "kombucha", 24);
+		prof.direBonjour();
+		prof.acheter( "kombucha", 12);
+		prof.acheter( "kombucha", 11);
 		
-//		Commerçant marco= new Commerçant("Marco",15);
-//		marco.direBonjonr();
-//		marco.seFaireExtorquer();
-//		marco.recevoir(15);
-//		marco.boire();
-	
+		System.out.println("---------------------------------------");
+		
+		Commerçant marco= new Commerçant("Marco",15);
+		marco.direBonjour();
+		marco.seFaireExtorquer();
+		marco.recevoir(15);
+		marco.boire();
+		
+		System.out.println("---------------------------------------");
+		
 		Yakuza yaku= new Yakuza("Yaku Le Noir", 30, "Warsong", 2);
-//		yaku.direBonjour();
-//		yaku.extorquer(marco);
+		yaku.direBonjour();
+		yaku.extorquer(marco);
+		
+		System.out.println("---------------------------------------");
 		
 		Ronin roro = new Ronin("Roro", 60, 2);
 		roro.provoquer(yaku);
